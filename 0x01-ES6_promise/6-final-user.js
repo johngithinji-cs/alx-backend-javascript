c6f3267ee010b4a851531aa8f01d4f45d31683a5
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-12-20 16:37:41 +0300

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,13 @@
-import signUpUser from './4-user-promise.js'
-import uploadPhoto from './5-photo-reject.js'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-	return Promise
-	.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-	.then((res) => (
-		res.map((j) => ({
-			status: j.status,
-			value: j.status === 'fulfilled' ? j.value : String(j.reason),
-		}))
-	));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
