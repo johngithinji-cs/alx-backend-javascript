e2e96f32e7a41c645ff78aba240322da49c4d60b
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-12-22 14:57:27 +0300

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,13 +1,13 @@
 /**
- * Represents a classroom
+ * Represents a class room.
  */
-export default class ClassRoom{
-	/**
-	 * Creates a new @see {@link ClassRoom}.
-	 * @param {Number} maxStudentsSize - The maximum number of
-	 * students in the class.
-	 */
-	constructor(maxStudentsSize){
-		this._maxStudentsSize = maxStudentsSize
-	}
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
