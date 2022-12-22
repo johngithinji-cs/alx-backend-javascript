c4d86eac259816b271aca677ab84de87b4ebe82c
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-12-22 14:52:12 +0300

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,41 +1,41 @@
 export default class Car {
-	  constructor(brand, motor, color) {
-		      this.brand = brand;
-		      this.motor = motor;
-		      this.color = color;
-		    }
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
 
-	  get brand() {
-		      return this._brand;
-		    }
+  get brand() {
+    return this._brand;
+  }
 
-	  set brand(value) {
-		      this._brand = value;
-		    }
+  set brand(value) {
+    this._brand = value;
+  }
 
-	  get motor() {
-		      return this._motor;
-		    }
+  get motor() {
+    return this._motor;
+  }
 
-	  set motor(value) {
-		      this._motor = value;
-		    }
+  set motor(value) {
+    this._motor = value;
+  }
 
-	  get color() {
-		      return this._color;
-		    }
+  get color() {
+    return this._color;
+  }
 
-	  set color(value) {
-		      this._color = value;
-		    }
+  set color(value) {
+    this._color = value;
+  }
 
-	  static get [Symbol.species]() {
-		      return this;
-		    }
+  static get [Symbol.species]() {
+    return this;
+  }
 
-	  cloneCar() {
-		      const Species = this.constructor[Symbol.species];
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
 
-		      return new Species();
-		    }
+    return new Species();
+  }
 }
