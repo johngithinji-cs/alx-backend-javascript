0a7fbd5db9de8e32b998ddf15b5ec160a081541f
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-12-22 14:53:15 +0300

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,32 +1,32 @@
 export default class HolbertonClass {
-	  constructor(size, location) {
-		      this.size = size;
-		      this.location = location;
-		    }
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
 
-	  get size() {
-		      return this._size;
-		    }
+  get size() {
+    return this._size;
+  }
 
-	  set size(value) {
-		      this._size = value;
-		    }
+  set size(value) {
+    this._size = value;
+  }
 
-	  get location() {
-		      return this._location;
-		    }
+  get location() {
+    return this._location;
+  }
 
-	  set location(value) {
-		      this._location = value;
-		    }
+  set location(value) {
+    this._location = value;
+  }
 
-	  [Symbol.toPrimitive](hint) {
-		      if (hint === 'number') {
-			            return this.size;
-			          }
-		      if (hint === 'string') {
-			            return this.location;
-			          }
-		      return this;
-		    }
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
+  }
 }
