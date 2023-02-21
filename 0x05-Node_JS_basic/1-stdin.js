b63f0766d79a85477c264ec97566252cf3d5bc34
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-02-21 19:48:18 +0300

    minor changes
    minor changes

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,8 +2,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
     const chunk = process.stdin.read();
+    
     if (chunk) {
-      process.stdout.write('Your name is:  + ${chunk}');
+      process.stdout.write(`Your name is:  ${chunk}`);
     }
   });
 
