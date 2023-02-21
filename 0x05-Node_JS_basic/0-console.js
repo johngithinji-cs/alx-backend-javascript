2db13eab84d26d663aad82bcbf3cfde875b30414
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-02-21 18:58:50 +0300

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,6 @@
 /**
  * Displays a message on the STDOUT.
  * @param {string}msg the message to display
- * @author John Mathai Githinji
  */
 const displayMessage = (msg) => {
     console.log(msg);
