cace18bd064066f6a6f99bafdc190274aaf1c3f0
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-02-21 19:40:48 +0300

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(string) {
-	console.log(string);
-}
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author John Mathai <https://github.com/johngithinji-cs>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
 module.exports = displayMessage;
