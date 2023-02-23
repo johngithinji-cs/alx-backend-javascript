dee1d04d730dc2c520d698e8bd3e4073e905acce
johngithinji-cs <mathaijohn97@gmail.com>
2023-02-23 11:54:57 +0300

    Passes lint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,35 +7,34 @@ const fs = require('fs')
  */
 
 const countStudents = (path) => {
-    if(!fs.existsSync(path)){
-        throw new Error('Cannot load the database');
-    }
-    if(!fs.statSync(path).isFile())
-    {
-        throw new Error('Cannot load the database');
-    }
-    const extractLines = fs
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const extractLines = fs
     .readFileSync(path, 'utf-8')
     .toString('utf-8')
     .trim()
     .split('\n');
-const studentGroup = {};
-const fieldNames = extractLines[0].split(',');
-const studentPropNames = fieldNames.slice(0, fieldNames.length - 1);
+  const studentGroup = {};
+  const fieldNames = extractLines[0].split(',');
+  const studentPropNames = fieldNames.slice(0, fieldNames.length - 1);
 
-for (const line of extractLines.slice(1)) {
+  for (const line of extractLines.slice(1)) {
     const studentRecord = line.split(',');
-    const studentValues = studentRecord.slice(0, studentRecord.length -1);
+    const studentValues = studentRecord.slice(0, studentRecord.length - 1);
     const fields = studentRecord[studentRecord.length - 1];
 
     if (!Object.keys(studentGroup).includes(fields)) {
-        studentGroup[fields] = [];
-      }
+      studentGroup[fields] = [];
+    }
     const studentEntries = studentPropNames
-    .map((propName, idx) => [propName, studentValues[idx]]);
-    studentGroup[fields].push(Object.fromEntries(studentEntries));   
-}
-const totalStudents = Object
+      .map((propName, idx) => [propName, studentValues[idx]]);
+    studentGroup[fields].push(Object.fromEntries(studentEntries));
+  }
+  const totalStudents = Object
     .values(studentGroup)
     .reduce((pre, cur) => (pre || []).length + cur.length);
   console.log(`Number of students: ${totalStudents}`);
