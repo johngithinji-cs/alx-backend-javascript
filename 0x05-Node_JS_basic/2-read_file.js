d92c9dc4760d046cf4705f20b47dbbfa8d4f2a81
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-02-23 12:23:49 +0300

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,20 +3,25 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author John Githinji <https://github.com/johngithinji-cs>
+ * @throws {Error} If the database cannot be loaded.
+ * @returns {void}
+ * @see {@link https://github.com/johngithinji-cs|John Githinji}
  */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
     throw new Error('Cannot load the database');
   }
+
   if (!fs.statSync(dataPath).isFile()) {
     throw new Error('Cannot load the database');
   }
+
   const fileLines = fs
     .readFileSync(dataPath, 'utf-8')
     .toString('utf-8')
     .trim()
     .split('\n');
+
   const studentGroups = {};
   const dbFieldNames = fileLines[0].split(',');
   const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
@@ -25,18 +30,23 @@ const countStudents = (dataPath) => {
     const studentRecord = line.split(',');
     const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
     const field = studentRecord[studentRecord.length - 1];
+
     if (!Object.keys(studentGroups).includes(field)) {
       studentGroups[field] = [];
     }
+
     const studentEntries = studentPropNames
       .map((propName, idx) => [propName, studentPropValues[idx]]);
+
     studentGroups[field].push(Object.fromEntries(studentEntries));
   }
 
   const totalStudents = Object
     .values(studentGroups)
     .reduce((pre, cur) => (pre || []).length + cur.length);
+
   console.log(`Number of students: ${totalStudents}`);
+
   for (const [field, group] of Object.entries(studentGroups)) {
     const studentNames = group.map((student) => student.firstname).join(', ');
     console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
