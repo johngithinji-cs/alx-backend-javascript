626ea52fb5e7caf88330fb96befe40c56785df93
johngithinji-cs <mathaijohn97@gmail.com>
2023-02-23 11:50:39 +0300

    new trial

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-<<<<<<< HEAD
   const chunk = process.stdin.read();
 
   if (chunk) {
@@ -12,14 +11,3 @@ process.stdin.on('readable', () => {
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-=======
-	const name = process.stdin.read();
-	if (name) {
-		process.stdout.write(`Your name is: ${name}`);
-	}
-});
-
-process.stdin.on('end', () => {
-	process.stdout.write('This important software is now closing\n');
-});
->>>>>>> f6a209a59af326e99c88214bd764021e86bc97a7
