664942e49c4198733a5932e7c071ef16df247a06
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-02-23 12:26:46 +0300

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,9 +3,14 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @throws {Error} If the database cannot be loaded.
- * @returns {void}
- * @see {@link https://github.com/johngithinji-cs|John Githinji}
+ * @returns {undefined}
+ * @throws {Error} if the dataPath does not exist or is not a file.
+ * @throws {Error} if the database cannot be loaded.
+ * @example
+ * countStudents('./database.csv');
+ * // Number of students: 64
+ * // Number of students in Engineering: 24. List: John Doe, Jane Doe, ...
+ * // Number of students in Math: 20. List: Alice, Bob, ...
  */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
@@ -16,9 +21,8 @@ const countStudents = (dataPath) => {
     throw new Error('Cannot load the database');
   }
 
-  const fileLines = fs
-    .readFileSync(dataPath, 'utf-8')
-    .toString('utf-8')
+  const fileLines = fs.readFileSync(dataPath, 'utf-8')
+    .toString()
     .trim()
     .split('\n');
 
@@ -30,20 +34,16 @@ const countStudents = (dataPath) => {
     const studentRecord = line.split(',');
     const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
     const field = studentRecord[studentRecord.length - 1];
-
     if (!Object.keys(studentGroups).includes(field)) {
       studentGroups[field] = [];
     }
-
-    const studentEntries = studentPropNames
-      .map((propName, idx) => [propName, studentPropValues[idx]]);
-
+    const studentEntries = studentPropNames.map((propName, idx) => [propName, studentPropValues[idx]]);
     studentGroups[field].push(Object.fromEntries(studentEntries));
   }
 
-  const totalStudents = Object
-    .values(studentGroups)
-    .reduce((pre, cur) => (pre || []).length + cur.length);
+  const totalStudents = Object.values(studentGroups)
+    .reduce((pre, cur) => pre.length + cur.length, [])
+    .length;
 
   console.log(`Number of students: ${totalStudents}`);
 
