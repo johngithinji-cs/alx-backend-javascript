7f565551ec4fee7925f681afd1ee7a85f8df8c8e
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-02-24 07:29:52 +0300

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,14 +1,36 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-  it('should return the sum of two rounded numbers', function() {
-    assert.strictEqual(calculateNumber(1.2, 3.8), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.5), 5);
-    assert.strictEqual(calculateNumber(1.9, 3.1), 5);
-    assert.strictEqual(calculateNumber(2.49, 2.51), 5);
-    assert.strictEqual(calculateNumber(2.49, 2.5), 5);
-    assert.strictEqual(calculateNumber(2.5, 2.51), 5);
-    assert.strictEqual(calculateNumber(2.49999, 2.50001), 5);
+describe('calculateNumber', () => {
+  it('should return the sum of two whole numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
+  });
+
+  it('should round down b\'s fractional number and return the sum', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
+  });
+
+  it('should round down a and b\'s fractional number and return the sum', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
+  });
+
+  it('should round down a\'s fractional number and return the sum', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+  });
+
+  it('should round up b\'s fractional number and return the sum', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
+  });
+
+  it('should round up a and b\'s fractional number and return the sum', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
+  });
+
+  it('should round up a\'s fractional number and return the sum', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+  });
+
+  it('should round down a and b fractional numbers with trailing 9\'s and return the sum', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
 });
